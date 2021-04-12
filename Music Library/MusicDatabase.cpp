f1ce7447c23b49451981a808a0927419dc7364e1
Pranav Chatur <pranavchatur@gmail.com>
2021-04-12 15:47:27 +0530

    Added AddSong Program showing some unknown error but runs perfectly fine Some issues with printing the rating of the song to be looked into

--- Music Library/MusicDatabase.cpp
+++ Music Library/MusicDatabase.cpp
@@ -2,20 +2,41 @@
 #include <string>
 using namespace std;
 
-class Song {
+class Songs{
     private:
         int rating;
     public:
         string name;
         string artist;
         string album;
-        bool masterpiece(){     //To change definition of Masterpiece
-            if (rating >= 8){
-                return true;
-            }
-            return false;
+
+        Songs(string s_name,string s_album,string s_artist){
+            name = s_name;album = s_album;artist = s_artist;
         }
+
+        void setrating(int s_rating){
+            if(s_rating<0||s_rating>10){
+                cout<<"INVALID INPUT\nEnter a value between 1-10 only\n";
+                rating = 0;
+            }
+            else{
+                rating = s_rating;
+            }
+        }
+        /*string getrating(){
+            return rating;
+        }*/
+
 };
+
+void display_song(Songs song){
+    cout<<"Name: "<<song.name<<endl;
+    cout<<"Album: "<<song.album<<endl;
+    cout<<"Artist: "<<song.artist<<endl;
+    //cout<<"Rated:"<<song.getrating()<<endl;
+}
+
+void AddSong();
 
 void choice(){
     cout<<"\nMENU\n";
@@ -27,6 +48,8 @@ void choice(){
     cin>>x;
     switch (x) {
         case 1:
+            AddSong();
+            choice();
             break;
         case 4:
             exit(0);
@@ -35,11 +58,26 @@ void choice(){
             cout<<"INVALID CHOICE";
             choice();
     }
-
 }
+
 
 
 int main(){
     choice();
     return 0;
+}
+
+void AddSong(){
+    string song_name, song_album, song_artist;
+    int song_rating;
+    cout<<"Enter the name, album & artist of the song\n";
+    cin.ignore();
+    getline(cin,song_name);getline(cin,song_album);getline(cin,song_artist);
+    Songs song_1(song_name, song_album, song_artist); //Modify later so that it can take in more songs
+    cout<<"\nEnter the rating from 1-10\n";
+    cin>>song_rating;
+    song_1.setrating(song_rating);
+    cout<<"Entry Added\n";
+    display_song(song_1);
+    choice();
 }
