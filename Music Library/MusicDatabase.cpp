ab002e38338dc32bbcc33d311eaac391a33c5b05
Pranav Chatur <pranavchatur@gmail.com>
2021-04-12 14:37:43 +0530

    Added void choice

--- Music Library/MusicDatabase.cpp
+++ Music Library/MusicDatabase.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 class Song {
+    private:
+        int rating;
     public:
         string name;
         string artist;
         string album;
-        int rating;
-        //To change definition of Masterpiece
-        bool masterpiece(){
+        bool masterpiece(){     //To change definition of Masterpiece
             if (rating >= 8){
                 return true;
             }
@@ -17,7 +17,29 @@ class Song {
         }
 };
 
-int main(){
+void choice(){
+    cout<<"\nMENU\n";
+    cout<<"Press 1 to register a new entry\n";
+    cout<<"Press 2 to view existing entries\n";
+    cout<<"Press 3 to change existing entries\n";
+    cout<<"Press 4 to exit the program\n";
+    int x;
+    cin>>x;
+    switch (x) {
+        case 1:
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            cout<<"INVALID CHOICE";
+            choice();
+    }
 
+}
+
+
+int main(){
+    choice();
     return 0;
 }
