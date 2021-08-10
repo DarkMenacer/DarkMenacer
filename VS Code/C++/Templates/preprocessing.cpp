74ff33d7ac96bb9fe6a7f153dd8e6c9812ab1abd
Pranav Chatur <pranavchatur@gmail.com>
2021-08-10 09:27:15 +0530

    Initial Commit

---
+++ VS Code/C++/Templates/preprocessing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>   // set and multiset
+#include <map>   // map and multimap
+#include <unordered_set>  // unordered set/multiset
+#include <unordered_map>  // unordered map/multimap
+#include <iterator>
+#include <algorithm>
+#include <numeric>    // some numeric algorithm
+#include <functional>
+#define FOR(i,start,limit) for(i=start;i<limit;++i)
+
+#define fastIO ios_base::sync_with_stdio(0 && cin.tie(0) && cout.tie(0));
+int32_t mod = 1e9 + 7;
+void solve()
+{/* Code here*/}
+
+int32_t main()
+{
+    fastIO;
+    solve();
+    return 0;
+}
