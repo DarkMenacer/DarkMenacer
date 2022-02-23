9636537a373162c7459049935913cffc017bbc27
Pranav Chatur <pranavchatur@gmail.com>
2022-02-24 02:38:24 +0530

    Memorable Smeet Conversation

--- VS Code/C++/CSES/Maximum_Subarray_sum.cpp
+++ VS Code/C++/CSES/Maximum_Subarray_sum.cpp
@@ -1,13 +1,14 @@
 /*
 Name:-              Maximum Subarray Sum
 Platform:-          CSES
-Date:-              20 January 2022
-Type of Problem:-   Searching, (Greedy?), (Sorting?)
+Date:-              20 February 2022
+Type of Problem:-   Greedy, incremental design
 Complexity:-        O(n)
-Status:-            Wrong Answer (Wrong Algorithm)
+Status:-            Solved
 
 Solution in brief:
--
+For every succesive element in the 'values' array, we compare existing (sum + next) and (next) itself, and 
+
 Sample to understand solution:
 Input:-
 
@@ -21,36 +22,23 @@ Output:-
 #include <numeric>
 #define FOR(i,start,upper_limit) for(i=start;i<upper_limit;++i)
 #define pb push_back
+#define ll long long
 using namespace std;
  
-int give_me_answer(vector<int> values){
-    int answer = 0, sum = 0, new_sum = 0;
-    vector<int>::iterator start = values.begin(), stop = values.begin();
-    while(1){
-        while(*start<0 && start < stop){++start;}
-        sum = accumulate(start,stop,0);
-        answer = max(answer,sum);
-        if(stop == values.end()){
-            if(start == stop){break;}
-            ++start;
-            sum = accumulate(start,stop,0);
-            answer = max(answer,sum);
-            continue;
-        }
-        ++stop;
-        new_sum = accumulate(start,stop,0);
-        /* cout<<"start: "<<*start<<" & "<<"stop: "<<*stop<<" & ";
-        cout<<"sum: "<<sum<<" & ";
-        cout<<"new_sum: "<<new_sum<<"\n"; */
-        if(new_sum < sum && new_sum < 0){++start;}
+ll give_me_answer(vector<ll> &values){
+    ll max_sum = values[0], sum = max_sum;
+    unsigned i;
+    FOR(i,1,values.size()){
+        sum = max(sum + values[i], values[i]);
+        max_sum = max(max_sum, sum);
     }
-    return answer;
+    return max_sum;
 }
  
 int main(){
     int n,i,x;
     cin>>n;
-    vector<int> values;
+    vector<ll> values;
     FOR(i,0,n){
         cin>>x;
         values.pb(x);
